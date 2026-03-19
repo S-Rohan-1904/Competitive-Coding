86a72272fcb509686112cd54b6b83987d466fcd1
S-Rohan-1904 <140544793+S-Rohan-1904@users.noreply.github.com>
2026-03-19 23:30:01 +0530

    Auto-update: 2026-03-19 23:30:01

--- practice/0-1bfs.cpp
+++ practice/0-1bfs.cpp
@@ -1,16 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
-vi dist;
-using pii = pair<int,int>;
-void bfs01(int start) {
-    deque<pii> dq;
-    dq.push_back({0, start});
-    dist[start] = 0;
 
-    while(!dq.empty()) {
-        pii front = dq.front();
+#define rep(i, l, r) for (int i = l; i < r; i++)
+#define rrep(i, r, l) for (int i = r-1; i >=0; i--)
+#define ll long long
+#define nline '\n'
+#define prn cout << "NO" << nline
+#define pry cout << "YES" << nline
+#define vi vector<int>
+#define vvi vector<vi>
+#define eb emplace_back
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define inparr(v)   \
+  for (auto &x : v) \
+    cin >> x;
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vpii vector<pii>
+#define mp make_pair
+#define ff first
+#define ss second
+int n, m;
+
+// Corrected types: 
+// dist: stores shortest path
+// g: vector of vectors of pairs {neighbor, weight}
+vi dist;
+vector<vector<pii>> g; 
+
+void zero_one_bfs(int start) {
+    deque<int> dq;
+
+    dist[start] = 0;
+    dq.push_back(start);
+
+    while (!dq.empty()) {
+        int u = dq.front();
         dq.pop_front();
 
+        for (auto &edge : g[u]) {
+            int v = edge.ff;
+            int weight = edge.ss;
 
+            if (dist[u] + weight < dist[v]) {
+                dist[v] = dist[u] + weight;
+                
+                if (weight == 0) {
+                    dq.push_front(v);
+                } else {
+                    dq.push_back(v);
+                }
+            }
+        }
     }
 }
