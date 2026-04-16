55b0b964f1bed4e5016a2dd87870c6f2f54e3c6f
S-Rohan-1904 <140544793+S-Rohan-1904@users.noreply.github.com>
2026-04-16 23:30:00 +0530

    Auto-update: 2026-04-16 23:30:00

--- practice/Message_Route.cpp
+++ practice/Message_Route.cpp
@@ -1,79 +1,114 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define rep(i, l, r) for (int i = l; i < r; i++)
-#define eb emplace_back
+#define rrep(i, r, l) for (int i = r-1; i >=0; i--)
+#define ll long long
 #define nline '\n'
+#define prn cout << "NO" << nline
+#define pry cout << "YES" << nline
 #define vi vector<int>
 #define vvi vector<vi>
+#define eb emplace_back
 #define all(a) (a).begin(), (a).end()
-
+#define rall(a) (a).rbegin(), (a).rend()
+#define inparr(v)   \
+  for (auto &x : v) \
+    cin >> x;
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vpii vector<pii>
+#define vvpii vector<vector<pii>>
+#define mp make_pair
+#define ff first
+#define ss second
+#define float long double
+ 
+// #define int long long
+ 
+//---- Debugger ---- //
+#ifdef LOCAL
+#define debarr(a,n) cout<<#a<<" : ";for(int i=0;i<n;i++) cerr<<a[i]<<" "; cerr<<nline;
+#define debmat(mat,row,col) cout<<#mat<<" :\n";for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cerr<<mat[i][j]<<" ";cerr<<nline;}
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__)
+template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ")";}
+template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const unordered_set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const multiset<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> void dbs(string str, T t) {cerr << str << " : " << t << "\n";}
+template <class T, class... S> void dbs(string str, T t, S... s) {int idx = str.find(','); cerr << str.substr(0, idx) << " : " << t << ","; dbs(str.substr(idx + 1), s...);}
+template <class T> void prc(T a, T b) {cerr << "["; for (T i = a; i != b; ++i) {if (i != a) cerr << ", "; cerr << *i;} cerr << "]\n";}
+#else
+#define pr(...)
+#endif
+//----------------- //
+ 
+const int MOD = 1e9 + 7;
 const int INF = 1e9 + 1;
-
-int n, m;
+ 
+int n,m;
 vvi g;
 vi vis;
 vi dist;
 vi par;
-
-void bfs(int start_node) {
-    queue<int> q;
-    q.push(start_node);
-    dist[start_node] = 0;
-    par[start_node] = INF;
-    vis[start_node] = 1;
-    while(!q.empty()) {
-        int front = q.front();
-        q.pop();
-
-        for(auto v : g[front]) {
-            if(!vis[v]) {
-              vis[v] = 1;
-              q.push(v);
-              par[v] = front;
-              dist[v] = dist[front] + 1;
-            }
-        } 
+ 
+void bfs(int start) {
+  dist[start] = 0;
+  par[start] = INF;
+  queue<int> q;
+  q.push(start);
+ 
+  while(!q.empty()) {
+    int front = q.front();
+    q.pop();
+ 
+    if(vis[front]) continue;
+    vis[front] = 1;
+ 
+    for(auto & child : g[front]) {
+      if(!vis[child]) {
+        q.push(child);
+        par[child] = front;
+        dist[child] = dist[front] + 1;
+      }
     }
+  }
 }
-
-
 void solve()
 {
-  cin >> n >> m;
-  
-  g.assign(n + 1, vi());
-  vis.assign(n + 1, 0);
-  dist.assign(n + 1, INF);
-  par.assign(n + 1, 0);
-
-  rep(i, 0, m) {
-    int u, v;
-    cin >> u >> v;
+  cin>>n>>m;
+  // pr(n,m);
+  g.resize(n+1);
+  vis.assign(n+1, 0);
+  dist.assign(n+1, INF);
+  par.resize(n+1);
+  rep(i,0,m) {
+    int u,v;
+    cin>>u>>v;
     g[u].eb(v);
     g[v].eb(u);
   }
-
+  pr(g);
   bfs(1);
-
-  if(dist[n] >= INF) {
-    cout << "IMPOSSIBLE" << nline;
-  } else {
-    cout << dist[n] + 1 << nline;
+  pr(dist);
+  if(dist[n] >= INF) cout<<"IMPOSSIBLE";
+  else {
+    cout<<dist[n] + 1<<nline;
     int curr = n;
     vi path;
-    
     while(curr != INF) {
       path.eb(curr);
       curr = par[curr];
     }
-    
     reverse(all(path));
-    for(auto &el : path) cout << el << " ";
-    cout << nline;
+    for(auto &el : path) cout<<el<<" ";
+    cout<<nline;
   }
 }
-
+ 
 signed main()
 {
   ios_base::sync_with_stdio(0);
