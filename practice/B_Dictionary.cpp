4206635f49642e3810b581a59b2fc656fb71fb6a
S-Rohan-1904 <140544793+S-Rohan-1904@users.noreply.github.com>
2026-05-09 23:30:00 +0530

    Auto-update: 2026-05-09 23:30:00

--- practice/B_Dictionary.cpp
+++ practice/B_Dictionary.cpp
@@ -28,7 +28,6 @@ vi sieve(int n)
     vi primes;
     vector<bool> is_prime(n + 1, true);
     is_prime[0] = is_prime[1] = false;
-
     for (int i = 2; i <= n; i++)
     {
         if (is_prime[i])
