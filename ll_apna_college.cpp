d3d4cde104945d651a2ff2d9ebaee60994e967bc
S-Rohan-1904 <140544793+S-Rohan-1904@users.noreply.github.com>
2026-07-15 23:30:01 +0530

    Auto-update: 2026-07-15 23:30:01

--- ll_apna_college.cpp
+++ ll_apna_college.cpp
@@ -23,8 +23,51 @@ public:
 
     void push_front(int val) {
         Node* newNode = new Node(val);
-        if(head == nullptr) {
-            
+        if( head == nullptr ) {
+            head = tail = newNode;
+            return;
         }
+        newNode->next = head;
+        head = newNode;
+    }
+    
+    void push_back(int val) {
+        Node* newNode = new Node(val);
+        if( head == nullptr ) {
+            head = tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        tail = newNode;
+    }
+
+    void printLL() {
+        Node* temp = head;
+        while( temp != nullptr ) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+    void pop_front() {
+        if(head == nullptr) return;
+        Node* oldHead = head;
+        head = head->next;
+        delete oldHead;
+    }
+
+    void pop_back() {
+        if(head == nullptr) return;
+
+        Node* temp = head;
+
+        while (temp->next != tail) {
+            temp = temp->next;
+        }
+        temp->next = nullptr;
+        delete tail;
+        tail = temp;
+        
     }
 };
