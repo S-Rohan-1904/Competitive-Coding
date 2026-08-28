656424dab89bd2cb4de890e53c485ed70bae4a2c
S-Rohan-1904 <140544793+S-Rohan-1904@users.noreply.github.com>
2026-08-28 23:30:02 +0530

    Auto-update: 2026-08-28 23:30:01

---
+++ practice/B_NIT_Destroys_the_Universe.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, l, r) for (int i = l; i < r; i++)
+#define rrep(i, r, l) for (int i = r-1; i >=0; i--)
+#define ll long long
+#define nline '\n'
+#define prn cout << "NO" << nline
+#define pry cout << "YES" << nline
+#define vi vector<int>
+#define vvi vector<vi>
+#define eb emplace_back
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define inparr(v)   \
+  for (auto &x : v) \
+    cin >> x;
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vpii vector<pii>
+#define vvpii vector<vector<pii>>
+#define mp make_pair
+#define ff first
+#define ss second
+#define float long double
+
+// #define int long long
+
+//---- Debugger ---- //
+#ifdef LOCAL
+#define debarr(a,n) cout<<#a<<" : ";for(int i=0;i<n;i++) cerr<<a[i]<<" "; cerr<<nline;
+#define debmat(mat,row,col) cout<<#mat<<" :\n";for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cerr<<mat[i][j]<<" ";cerr<<nline;}
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__)
+template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ")";}
+template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const unordered_set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const multiset<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> void dbs(string str, T t) {cerr << str << " : " << t << "\n";}
+template <class T, class... S> void dbs(string str, T t, S... s) {int idx = str.find(','); cerr << str.substr(0, idx) << " : " << t << ","; dbs(str.substr(idx + 1), s...);}
+template <class T> void prc(T a, T b) {cerr << "["; for (T i = a; i != b; ++i) {if (i != a) cerr << ", "; cerr << *i;} cerr << "]\n";}
+#else
+#define pr(...)
+#endif
+//----------------- //
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 1;
+
+/*
+
+We can always do it in 2 moves -> choose the entire array twice
+If the array doesn't contain a zero then the ans is always 1
+If the array contains zero 
+
+1 2 3 4 5 6 7
+0 2 3 0 1 2 0
+0 2 3 1 1 2 0 -> l,r = 4
+0 0 0 0 0 0 0
+
+Ok so we need to make a range such that it doesn't contain 
+we don;t care abt traling and leading zeros,
+
+the ans is number of contiguous non zero elements
+*/
+
+void solve() {
+    int n;
+    cin>>n;
+    vi arr(n);
+    inparr(arr);
+
+    int start = 0;
+    while(start < n && arr[start] == 0) start++;
+
+    if(start == n) {
+        cout << 0 << nline;
+        return;
+    }
+
+    while(!arr.empty() && arr.back() == 0) arr.pop_back();
+
+    n = arr.size();
+    int ans = 0;
+
+    rep(i,start,n) {
+        if(arr[i] == 0) ans++;
+    }
+
+    cout << min(2, ans + 1) << nline;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+}
