17ba5105049d6ef81b384faf7c3f4d69bd2ecd6c
S-Rohan-1904 <140544793+S-Rohan-1904@users.noreply.github.com>
2026-09-18 23:30:01 +0530

    Auto-update: 2026-09-18 23:30:01

---
+++ practice/D_Yaroslav_and_Productivity.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, l, r) for (int i = l; i < r; i++)
+#define rrep(i, r, l) for (int i = r; i >=0; i--)
+#define ll long long
+#define nline '\n'
+#define prn cout << "NO" << nline
+#define pry cout << "YES" << nline
+#define vi vector<int>
+#define vvi vector<vi>
+#define eb emplace_back
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define inparr(v)   \
+  for (auto &x : v) \
+    cin >> x;
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vpii vector<pii>
+#define vvpii vector<vector<pii>>
+#define mp make_pair
+#define ff first
+#define ss second
+#define float long double
+
+// #define int long long
+
+//---- Debugger ---- //
+#ifdef LOCAL
+#define debarr(a,n) cout<<#a<<" : ";for(int i=0;i<n;i++) cerr<<a[i]<<" "; cerr<<nline;
+#define debmat(mat,row,col) cout<<#mat<<" :\n";for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cerr<<mat[i][j]<<" ";cerr<<nline;}
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__)
+template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ")";}
+template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const unordered_set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const multiset<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> void dbs(string str, T t) {cerr << str << " : " << t << "\n";}
+template <class T, class... S> void dbs(string str, T t, S... s) {int idx = str.find(','); cerr << str.substr(0, idx) << " : " << t << ","; dbs(str.substr(idx + 1), s...);}
+template <class T> void prc(T a, T b) {cerr << "["; for (T i = a; i != b; ++i) {if (i != a) cerr << ", "; cerr << *i;} cerr << "]\n";}
+#else
+#define pr(...)
+#endif
+//----------------- //
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 1;
+
+/*
+we will ideally use it on negative elements, but think of it in this way if the final sum increases then it's good.
+But we can't greedily just pick all negative elements
+
+I am thinking of a case where greedy doesn;t work
+
+1 -2 -100 -3 1
+
+1, 2, 3
+
+i think we have to sort the operations
+
+we get a +100 by doing 3
+but before that we can apply 1 to make the first element negative and then apply 3.
+
+So it is proved that temporarily making it negative is better sometimes
+
+if we have sorted operations
+how to check if we need to apply it or not
+m is also 2e5 so we need O(M)
+
+-1 2 -3 4 -5
+1 3 5
+we can actually iterate in reverse order of b
+we when we pick 5
+we need the previous elements to increase the negative sum and decrease the positive sum
++ve - -ve -> -ve - +ve
+if +ve el becomes -ve -> -2*el + -ve - +ve
+if -ve el becomes +ve -> -2*el + -ve - +ve
+-2*el is the contribution
+so higher the number better it is
+
+
+there are only two groups of numbers +ve or -ve
+*/
+
+void solve() {
+    
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+}
